34a589e1be8586a243a8023468bbac1086234021
Miika Tiihonen <m95.tiihonen@gmail.com>
2024-03-23 14:50:03 +0200

    Crude way to demo getting recipe data

--- screens/AddNewRecipe.js
+++ screens/AddNewRecipe.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import { TextInput, View, Text, Button } from "react-native"
+import { TextInput, View, Text, Button, ScrollView } from "react-native"
 import { storeData, getData, generateSHA256, getAllKeys, removeAllKeys } from "../util/LocalStorageUtil"
 import RecipeForm from "../components/RecipeForm"
 import RecipeSchema from "../data/RecipeSchema.json"
@@ -33,7 +33,27 @@ export default AddNewRecipe = () => {
         ), {})
     }
 
-    const [allKeys, setAllKeys] = useState([])
+    const [allRecipes, setAllRecipes] = useState([])
+    const getAllLocalRecipes = async () => {
+        const recipes = []
+        try {
+            let keys = await getAllKeys()
+
+            // Convert null/undefined/singleton into an array
+            keys = !keys ? [] :
+                !Array.isArray(keys) ? keys = [keys] : keys
+
+            for (const key of keys) {
+                const recipe = await getData(key);
+                recipes.push(recipe);
+            }
+
+            setAllRecipes(recipes)
+        } catch (error) {
+            console.error("Error while fetching keys:", error)
+        }
+    }
+
     const inputChange = (key, text) => {
         setSchema(prevSchema => ({
             ...prevSchema,
@@ -43,18 +63,9 @@ export default AddNewRecipe = () => {
             }
         }))
     }
-    const getKeys = async () => {
-        let keys = []
-        try {
-            keys = await getAllKeys(); // Wait for the asynchronous getAllKeys function to complete
-        } catch (error) {
-            console.error("Error while fetching keys:", error);
-        }
-        setAllKeys(keys);
-    };
 
     return (
-        <View>
+        <ScrollView>
             <RecipeForm schema={schema} inputChange={inputChange} />
 
             <Text>SHA256: {generateSHA256(schema)}</Text>
@@ -62,16 +73,16 @@ export default AddNewRecipe = () => {
             <Button title="store" onPress={storeItem} />
             <Text>Last stored status: {storedStatus}</Text>
 
-            <Text>fetch all keys:</Text>
+            <Text>fetch all local recipes:</Text>
 
-            <Button title="fetch" onPress={getKeys} />
-            {allKeys.map((key, index) => (
-                <Text key={index}>
-                    key #{index}: {key}
+            <Button title="fetch" onPress={getAllLocalRecipes} />
+            {allRecipes.map((item, index) => (
+                <Text key={index} style={{ borderWidth: 2, padding: 5 }}>
+                    recipe #{index}: {JSON.stringify(item)}
                 </Text>
             ))}
 
             <Button title="remove all" onPress={removeAllKeys} />
-        </View>
+        </ScrollView>
     )
 }
