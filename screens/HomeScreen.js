b4276c5787d1595530bfa2501a877539626abd40
Miika Tiihonen <m95.tiihonen@gmail.com>
2024-04-08 21:40:09 +0300

    Render recipes from local storage

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,51 +3,17 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import Styles from '../styles/Styles';
 import { useNavigation } from '@react-navigation/native';
+import MyRecipes from './MyRecipes';
 
 export default function HomeScreen() {
     const navigation = useNavigation();
     return (
         <ScrollView>
-        <View style={Styles.container}>
-            <Text>Home Screen</Text>
+            <View style={Styles.container}>
+                <Text>Home Screen</Text>
 
-            <View style={Styles.CatalogRow}>
-                <View style={Styles.CatalogItem}>
-                    <Image
-                    source={require('../components/img.jpeg')}
-                    style={Styles.CatalogImage}
-                    onPress={() => navigation.navigate('Recipe')}/>
-                    <Text
-                    onPress={() => navigation.navigate('Recipe')}
-                    style={Styles.h3}
-                    >Resepti 1</Text>
-                </View>
-                <View style={Styles.CatalogItem}>
-                    <Image source={require('../components/img.jpeg')} style={Styles.CatalogImage} />
-                    <Text style={Styles.h3}>Resepti 1</Text>
-                </View>
+                <MyRecipes />
             </View>
-            <View style={Styles.CatalogRow}>
-                <View style={Styles.CatalogItem}>
-                    <Image source={require('../components/img.jpeg')} style={Styles.CatalogImage} />
-                    <Text style={Styles.h3}>Resepti 1</Text>
-                </View>
-                <View style={Styles.CatalogItem}>
-                    <Image source={require('../components/img.jpeg')} style={Styles.CatalogImage} />
-                    <Text style={Styles.h3}>Resepti 1</Text>
-                </View>
-            </View>
-            <View style={Styles.CatalogRow}>
-                <View style={Styles.CatalogItem}>
-                    <Image source={require('../components/img.jpeg')} style={Styles.CatalogImage} />
-                    <Text style={Styles.h3}>Resepti 1</Text>
-                </View>
-                <View style={Styles.CatalogItem}>
-                    <Image source={require('../components/img.jpeg')} style={Styles.CatalogImage} />
-                    <Text style={Styles.h3}>Resepti 1</Text>
-                </View>
-            </View>
-        </View>
         </ScrollView>
     );
 }
--- screens/MyRecipes.js
+++ screens/MyRecipes.js
@@ -1,10 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { storeData, getData, generateSHA256, getAllKeys, removeAllKeys } from "../util/LocalStorageUtil"
+import Styles from '../styles/Styles';
+import { useNavigation } from '@react-navigation/native';
 
 export default function MyRecipes() {
+    const navigation = useNavigation()
+    const [recipes, setRecipes] = useState([])
+    const [searchQuery, setSearchQuery] = useState('');
+    const filteredRecipes = recipes.filter((recipe) =>
+        recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+    const [allRecipes, setAllRecipes] = useState([])
+    useEffect(() => {
+        (async () => {
+            setRecipes(await getAllLocalRecipes())
+        })()
+    }, [])
+
+    const getAllLocalRecipes = async () => {
+        const recipes = []
+        try {
+            let keys = await getAllKeys()
+
+            // Convert null/undefined/singleton into an array
+            keys = !keys ? [] :
+                !Array.isArray(keys) ? keys = [keys] : keys
+
+            for (const key of keys) {
+                const recipe = await getData(key);
+                recipes.push(recipe);
+            }
+
+            return recipes
+        } catch (error) {
+            console.error("Error while fetching keys:", error)
+        }
+    }
+
+    const handleRecipePress = (recipe) => {
+        navigation.navigate('Recipe', { recipe });
+    }
+
     return (
         <View>
             <Text>omat reseptit</Text>
+
+            <TextInput
+                style={Styles.searchBar}
+                placeholder='Etsi reseptejä'
+                onChangeText={(text) => setSearchQuery(text)}
+                value={searchQuery}
+            />
+            {filteredRecipes.map((recipe, index) => (
+                <View key={index} style={Styles.container}>
+                    <TouchableOpacity onPress={() => handleRecipePress(recipe)}>
+                        <View>
+                            <Text style={Styles.DiscoverH3}>{recipe.name}</Text>
+                            <View style={Styles.DiscoverRow}>
+                                <View style={Styles.DiscoverItem}>
+                                    <TouchableOpacity onPress={() => handleRecipePress(recipe)}>
+                                        <Image
+                                            source={require('../components/img.jpeg')}
+                                            style={Styles.DiscoverImage}
+                                        />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            ))}
         </View>
     );
 }
