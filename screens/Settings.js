449d57c573ab34cb2799a95a1aa71de1fed5f71c
t2jami03 <123319187+t2jami03@users.noreply.github.com>
2024-04-17 15:10:05 +0300

    Update Settings.js

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { ScrollView, View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
-import { useTheme } from '../styles/ThemeContext'; 
+import { Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import Styles from '../styles/Styles';
+
+import { useTheme } from '../styles/ThemeContext'; 
 
 export default function Settings() {
     const { selectedTheme, toggleTheme } = useTheme(); 
 
     const handleThemeChange = (themeName) => {
-        toggleTheme(themeName); 
+        toggleTheme(themeName);
     };
 
     return (
@@ -29,7 +30,7 @@ export default function Settings() {
 
                 <Text style={[selectedTheme.h1, selectedTheme.vali]}>Tietoja Sovelluksesta</Text>
                 <Text style={[selectedTheme.h3, { textAlign: 'center', paddingBottom: 10 }]}>Sovellusversio:</Text>
-                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 20 }]}>v.1.05</Text>
+                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 20 }]}>v.1.06</Text>
                 <Text style={[selectedTheme.h3, { textAlign: 'center', paddingBottom: 10 }]}>Tekijät:</Text>
                 <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>Saana Lapinkangas</Text>
                 <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>Nico Jokelainen</Text>
@@ -39,7 +40,11 @@ export default function Settings() {
                 <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 20 }]}>TVTSPO Mobiilikehitysprojekti, kevät 2024, Ryhmä 5</Text>
 
                 <Text style={[selectedTheme.h3, { textAlign: 'center', paddingBottom: 10 }]}>Yhteistyökumppanit:</Text>
-                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>*lisätään tähän jotain liiketoimintasuunnitelman mukaan*</Text>
+                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>Valio</Text>
+                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>Pirkka</Text>
+                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>S-Ryhmä</Text>
+                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>Kesko</Text>
+                <Text style={[selectedTheme.txt, { textAlign: 'center', paddingBottom: 10 }]}>*kyseessä on kouluprojekti, eli yhteistyökumppanit ovat keksittyjä*</Text>
 
                 <Image
                     source={require('../components/logo_full.png')}
