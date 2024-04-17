60963abb6d686c03c99557da9929ba2c12af9161
t2jami03 <123319187+t2jami03@users.noreply.github.com>
2024-04-17 15:10:30 +0300

    Update Tips.js

--- screens/Tips.js
+++ screens/Tips.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import { firestore, collection, DISCOVER } from '../firebase/Config.js';
 import { query, onSnapshot, where, orderBy, startAfter, limit } from 'firebase/firestore';
-import Styles from '../styles/Styles';
 import { useNavigation } from '@react-navigation/native';
 
 import { useTheme } from '../styles/ThemeContext';
@@ -12,7 +11,7 @@ export default function Tips() {
     const navigation = useNavigation();
     const { selectedTheme } = useTheme(); 
 
-    const [discovers, setDiscovers] = useState([]);
+    const [discovers, setDiscovers] = useState([])
     const [searchQuery, setSearchQuery] = useState('');
 
     useEffect(() => {
@@ -28,7 +27,10 @@ export default function Tips() {
                 const discoverObject = {
                     id: doc.id,
                     instructions: doc.data().instructions,
-                    title: doc.data().title
+
+                    title: doc.data().title,
+                    image: doc.data().image
+
                 }
                 tempDiscovers.push(discoverObject);
             })
@@ -48,38 +50,43 @@ export default function Tips() {
     // ->      
     // <View key={index} style={[Styles.container, index !== 0 && Styles.recipeSeparator]}>
     // Styles.js -> DiscoverSeparator pois kommentista
-    const filteredDiscovers = discovers.filter((discover) =>
-    discover.title.toLowerCase().includes(searchQuery.toLowerCase())
-);
 
-return (
-<ScrollView>
-    <View style={selectedTheme.container}>
-        <TextInput
-            style={selectedTheme.searchBar}
-            placeholder='Etsi vinkkejä'
-            onChangeText={(text) => setSearchQuery(text)}
-            value={searchQuery}
-        />
-        <Text style={[selectedTheme.h1, selectedTheme.vali]}>Inspiraatiota ja vinkkejä</Text>
-        {filteredDiscovers.map((discover, index) => (
-            <View key={index}>
-                <TouchableOpacity onPress={() => handleDiscoverPress(discover)}>
-                    <View>
-                        <TouchableOpacity onPress={() => handleDiscoverPress(discover)}>
-                            <View style={selectedTheme.TipsImageContainer}>
-                                <Image
-                                    source={require('../components/img2.jpg')}
-                                    style={selectedTheme.TipsImage}
-                                />
-                            </View>
-                        </TouchableOpacity>
-                        <Text style={selectedTheme.DiscoverH3}>{discover.title}</Text>
-                    </View>
-                </TouchableOpacity>
+    const filteredDiscovers = discovers.filter((discover) =>
+        discover.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+    return (
+        <ScrollView style={selectedTheme.scrollview}>
+
+            <View style={selectedTheme.container}>
+
+            <TextInput
+                style={selectedTheme.searchBar}
+                placeholder='Etsi vinkkejä'
+                onChangeText={(text) => setSearchQuery(text)}
+                value={searchQuery}
+            />
+
+            <Text style={[selectedTheme.h1, selectedTheme.vali]}>Inspiraatiota ja vinkkejä</Text>
+            {filteredDiscovers.map((discover, index) => (
+                <View key={index}>
+                    <TouchableOpacity onPress={() => handleDiscoverPress(discover)}>
+                        <View>
+                                    <TouchableOpacity onPress={() => handleDiscoverPress(discover)}>
+                                        <View style={selectedTheme.TipsImageContainer}>
+                                        <Image
+                                            source={{ uri: discover.image }}
+                                            style={selectedTheme.TipsImage}
+                                        /></View>
+                                    </TouchableOpacity>
+                                    <Text style={selectedTheme.DiscoverH3}>{discover.title}</Text>
+                                </View>
+                    </TouchableOpacity>
+                </View>
+            ))}
             </View>
-        ))}
-    </View>
-</ScrollView>
-);
+
+        </ScrollView>
+    );
+    
 }
