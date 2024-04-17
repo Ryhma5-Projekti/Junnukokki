e89023afc04ac44f80fa95e838e05948ff034508
t2jami03 <123319187+t2jami03@users.noreply.github.com>
2024-04-17 15:05:50 +0300

    Update Navigation.js

--- Navigation.js
+++ Navigation.js
@@ -13,107 +13,78 @@ import Settings from './screens/Settings';
 import Recipe from './screens/Recipe';
 import OpenTips from './screens/OpenTips';
 
-import { useTheme } from './styles/ThemeContext'; 
+import { useTheme } from './styles/ThemeContext';
 
+// alkuperäinen navigaatio, joka toimii mutta välkkyy headerin latautuessa aina sivun latauksen yhteydessä...
+// välkkymistä havaittu applen puhelimella, androidilla ei ongelmia?
 
 export default function BottomNavigation() {
-    const Tab = createBottomTabNavigator();
-    const Stack = createStackNavigator();
-
-    function Header() {
-        const navigation = useNavigation();
-        const { selectedTheme } = useTheme(); 
-
-        return (
-            <SafeAreaView>
-                <View style={{
-                    height: 120,
-                    paddingHorizontal: 20,
-                    paddingTop: 15,
-                    width: '100%',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                    backgroundColor: selectedTheme.themeName === 'dark' ? '#2b2b2b' :
-                        selectedTheme.themeName === 'colorblind' ? 'white':
-                        selectedTheme.themeName === 'light' ? '#FAF5F1' : '#FAF5F1',
-                }}>
-                    <Image
-                        source={require('./components/logo_junnukokki_web.png')}
-                        style={{ width: '60%', height: '100%', resizeMode: 'contain' }}/>
-                    <FontAwesome5
-                        name="cog"
-                        size={28}
-                        color="#78B29C"
-                        onPress={() => navigation.navigate('Settings')}
-                    />
-                </View>
-            </SafeAreaView>
-        );
-    }
-
+    const Tab = createBottomTabNavigator()
+    const { selectedTheme } = useTheme();
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={{
-                    header: () => <Header />,
+        <>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={{
+                        header: () => <Header />,
                     tabBarStyle: {
-                        backgroundColor: '#78B29C',
+                        ...selectedTheme.bottomNavigationStyle, 
                         height: 100,
-                    },
-                    tabBarLabelStyle: {
-                        fontSize: 13,
-                        fontFamily: 'Exo',
-                        paddingBottom: 25,
-                    },
-                    tabBarActiveBackgroundColor: '#6A9C89',
-                    tabBarActiveTintColor: '#fff',   // aktiivinen tekstiväri
-                    tabBarInactiveTintColor: '#fff',    // inaktiivinen tekstiväri
-                }}
-            >
-                <Tab.Screen
-                    name="Stack"
-                    component={HomeStackScreen}
-                    options={{
-                        tabBarLabel: 'Reseptit',
-                        tabBarIcon: ({ color, size }) => (
-                            <FontAwesome5 name="book" size={24} color="white" />
-                        ),
+                        },
+                        tabBarLabelStyle: {
+                            fontSize: 13,
+                            fontFamily: 'Exo',
+                            paddingBottom: 25,
+                        },
+                        tabBarActiveBackgroundColor: selectedTheme.tabBarActiveBackgroundColor,
+                        tabBarActiveTintColor: '#fff',   // aktiivinen tekstiväri
+                        tabBarInactiveTintColor: '#fff',    // inaktiivinen tekstiväri
                     }}
-                />
-                <Tab.Screen
-                    name="AddNewRecipe"
-                    component={AddNewRecipe}
-                    options={{
-                        tabBarLabel: 'Lisää uusi',
-                        tabBarIcon: ({ color, size }) => (
-                            <FontAwesome5 name="plus" size={24} color="white" />
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="Tips"
-                    component={Tips}
-                    options={{
-                        tabBarLabel: 'Inspiroidu',
-                        tabBarIcon: ({ color, size }) => (
-                            <FontAwesome5 name="info-circle" size={24} color="white" />
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="Discover"
-                    component={Discover}
-                    options={{
-                        tabBarLabel: 'Kokeile',
-                        tabBarIcon: ({ color, size }) => (
-                            <FontAwesome5 name="list" size={24} color="white" />
-                        ),
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+                >
+                    <Tab.Screen
+                        name="Stack"
+                        component={HomeStackScreen}
+                        options={{
+                            tabBarLabel: 'Reseptit',
+                            tabBarIcon: ({ color, size }) => (
+                                <FontAwesome5 name="book" size={24} color="white" />
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="AddNewRecipe"
+                        component={AddNewRecipe}
+                        options={{
+                            tabBarLabel: 'Lisää uusi',
+                            tabBarIcon: ({ color, size }) => (
+                                <FontAwesome5 name="plus" size={24} color="white" />
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Tips"
+                        component={Tips}
+                        options={{
+                            tabBarLabel: 'Inspiroidu',
+                            tabBarIcon: ({ color, size }) => (
+                                <FontAwesome5 name="info-circle" size={24} color="white" />
+                            ),
+                        }}
+                    />
+                    <Tab.Screen
+                        name="Discover"
+                        component={Discover}
+                        options={{
+                            tabBarLabel: 'Kokeile',
+                            tabBarIcon: ({ color, size }) => (
+                                <FontAwesome5 name="list" size={24} color="white" />
+                            ),
+                        }}
+                    />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </>
+    )
 }
 
 // Tähän listataan kaikki screenit, jotka eivät näy alavalikossa
@@ -135,10 +106,41 @@ function HomeStackScreen() {
                 name="Recipe"
                 component={Recipe}
             />
-            <HomeStack.Screen
+             <HomeStack.Screen
                 name="OpenTips"
                 component={OpenTips}
             />
         </HomeStack.Navigator>
     )
+}
+
+// yläpalkki
+function Header() {
+    const navigation = useNavigation()
+    const { selectedTheme } = useTheme();
+
+    return (
+        <SafeAreaView>
+            <View style={[{
+                height: 120,
+                paddingHorizontal: 20,
+                paddingTop: 15,
+                width: '100%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+            }, selectedTheme.headerStyle]}>
+            
+                <Image
+                    source={require('./components/logo_junnukokki_web.png')}
+                    style={{ width: '60%', height: '100%', resizeMode: 'contain' }}/>
+                <FontAwesome5
+                    name="cog"
+                    size={28}
+                    color={selectedTheme.settingsColor} 
+                    onPress={() => navigation.navigate('Settings')}
+                />
+            </View>
+        </SafeAreaView>
+        )
 }
